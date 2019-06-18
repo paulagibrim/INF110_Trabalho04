c3a7861344e7fb535b699a347dd0a610bb684fca
Paula <paulagibrimufv@gmail.com>
2019-06-18 20:05:22 -0300

    .

---
+++ trabalho.cpp
@@ -0,0 +1,21 @@
+//***************************QUARTO TRABALHO DE INF110****************************//
+/*
+    TRABALHO ELABORADO PELOS ALUNOS DA DISCIPLINA INF 110 DA UNIVERSIDADE FEDERAL DE
+    VIÇOSA EM 2019/I.
+
+    GRUPO:  PAULA T M GIBRIM            ES90366                 ENGENHARIA MECÂNICA
+            HENRIQUE PADULA             ES?????                CIENCIA DA COMPUTAÇÃO
+
+    MODIFICAÇÃO: 12/06/2019
+*/
+
+#include <iostream>
+using namespace std;
+
+//**PARTE 1 - MAPA E PERSONAGENS**//
+
+//ESCOLHA DE MAPAS//
+int sel_mapa(){
+    cout << "Escolha o mapa"
+}
+
